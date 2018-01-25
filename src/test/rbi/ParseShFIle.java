96f09d0552afad346a7e2f2da087778817faffad
zhuanghua <tonyzhuanghua@gmail.com>
2018-01-25 21:14:07 +0000

    init

--- src/test/rbi/ParseShFIle.java
+++ src/test/rbi/ParseShFIle.java
@@ -3,7 +3,6 @@ package test.rbi;
 import java.io.BufferedReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,13 +11,13 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class ParseShFIle {
-    static Map<String, String> shmap = new HashMap<>(); // define a hashmap to save the variables, "key" is variable's name (left to "="), "value" is the value (right = "=")
+    private static Map<String, String> shmap = new HashMap<>(); // define a hashmap to save the variables, "key" is variable's name (left to "="), "value" is the value (on the right to "=")
 
 
     /**
- * readFile
- * Read in the file and parse the text, save to the variables and values into shmap
- * */
+     * readFile
+     * Read in the file and parse the text, save to the variables and values into shmap
+     */
     static private void readFile(String fileName) {
         List<String> list;
 
@@ -30,14 +29,14 @@ public class ParseShFIle {
                     .filter(line -> !line.startsWith("#") && !line.startsWith("echo") && !line.isEmpty() && !line.startsWith("java"))
                     .collect(Collectors.toList());
 
-            // Stream the list and parse the String, for each String in the list, find the "$*_*" replace with the value which has put into the map
+            // for each String in the list, parse the String, find the "$*_*" replace with the value which has put into the map
 
-            list.stream().forEach(line -> {
+            list.forEach(line -> {
 
                 String[] l = line.split("=");
                 String key = "", value = "";
                 if (!l[0].isEmpty()) key = l[0].trim();
-                if (l[1]!=null && !l[1].isEmpty()) value = l[1].trim();
+                if (l[1] != null && !l[1].isEmpty()) value = l[1].trim();
 
                 Pattern pattern = Pattern.compile("\\$([A-Za-z]\\w*)_(\\w+)"); //define a regex indicate start with $, followed by a valid character, a "_" in the middle, stopped by special character,
                 Matcher matcher = pattern.matcher(value);
@@ -59,9 +58,15 @@ public class ParseShFIle {
 
     public static void main(String[] args) {
 
-        // to be noted, this file is located relative path to project classpath, if move it to same folder with this java file, it should be changed to "src/test/rbi/script.sh"
-        ParseShFIle.readFile("script.sh");
-        System.out.println(shmap.get("CLASSPATH"));
+        // to be noted, this sh file is located relative path to project classpath, if move it to same folder with this java file, it should be changed to "src/test/rbi/script.sh"
+        final String filename = "script.sh";
+
+        ParseShFIle.readFile(filename);
+
+        String classPath = shmap.get("CLASSPATH");
+
+        if (classPath != null && !classPath.isEmpty())
+            System.out.println(classPath);
 
 
     }
